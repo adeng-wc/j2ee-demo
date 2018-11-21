8acbe3c57c186cfdd4af4863ef3a117d7daf5e90
翁某小小 <343577402@qq.com>
2018-11-21 14:07:22 +0800

    zxing 生成二维码图片

---
+++ data-structure/src/main/java/com/adeng/datastructure/BitSetTest.java
@@ -0,0 +1,20 @@
+package com.adeng.datastructure;
+
+import java.util.BitSet;
+
+/**
+ * @author hzwengcheng 2018-08-30 16:55
+ */
+public class BitSetTest {
+
+
+    public static void main(String[] args) {
+
+
+        BitSet bitSet = new BitSet(64);
+
+
+        System.out.println(bitSet);
+    }
+
+}
