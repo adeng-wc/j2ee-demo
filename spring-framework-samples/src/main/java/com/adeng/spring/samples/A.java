9c9692209014d399329da090e5f953faa93e7d8b
翁某小小 <343577402@qq.com>
2019-05-21 16:19:54 +0800

    spring 事件监听 demo

---
+++ spring-framework-samples/src/main/java/com/adeng/spring/samples/A.java
@@ -0,0 +1,7 @@
+package com.adeng.spring.samples;
+
+/**
+ * @author hzwengcheng 2019-05-08 14:50
+ */
+public class A {
+}
---
+++ spring-framework-samples/src/main/java/com/adeng/spring/samples/context/event/ApplicationListenerBeanOnCustomEventBootstrap.java
@@ -0,0 +1,46 @@
+package com.adeng.spring.samples.context.event;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.support.GenericApplicationContext;
+
+/**
+ *
+ * 监听自定义事件引导类，通过 {@link GenericApplicationContext#registerBean(Class, BeanDefinitionCustomizer...)} 方法注册
+ * {@link ApplicationListener} Bean
+ *
+ *
+ * @author hzwengcheng 2019-05-08 14:53
+ */
+public class ApplicationListenerBeanOnCustomEventBootstrap {
+
+
+    public static void main(String[] args) {
+        // 创建 Spring 应用上下文 GenericApplicationContext
+        GenericApplicationContext context = new GenericApplicationContext();
+        // 注册 ApplicationListener<MyApplicationEvent> 实现 MyApplicationListener
+        context.registerBean(MyApplicationListener.class); // registerBean 方法从 Spring 5 引入
+        // 初始化上下文
+        context.refresh();
+        // 发布自定义事件 MyApplicationEvent
+        context.publishEvent(new MyApplicationEvent("Hello World"));
+        // 关闭上下文
+        context.close();
+        // 再次发布事件
+        context.publishEvent(new MyApplicationEvent("Hello World Again"));
+    }
+
+    public static class MyApplicationEvent extends ApplicationEvent {
+        public MyApplicationEvent(String source) {
+            super(source);
+        }
+    }
+
+    public static class MyApplicationListener implements ApplicationListener<MyApplicationEvent> {
+
+        @Override
+        public void onApplicationEvent(MyApplicationEvent event) {
+            System.out.println(event.getClass().getSimpleName());
+        }
+    }
+}
